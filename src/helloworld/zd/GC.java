6541945ecb59d68a1de0ddc04cd8665ea0c4436a
Ross Khapilov <rossxpanteley@gmail.com>
2017-08-06 13:01:48 +0300

    Update

---
+++ src/helloworld/zd/GC.java
@@ -0,0 +1,21 @@
+package helloworld.zd;
+
+/**
+ * Created by User on 25.07.2017.
+ */
+public class GC {
+    private int size;
+
+    public static void main(String[] args) {
+        GC n = new GC();
+        n.setSize(1);
+        System.out.println("degfrew" + n.getSize());
+    }
+
+    public void setSize(int s) {
+        size = s;
+    }
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/helloworld/zd/zd2_2RecetIndexArrays.java
@@ -0,0 +1,8 @@
+package helloworld.zd;
+
+/**
+ * Created by User on 02.08.2017.
+ */
+public class zd2_2RecetIndexArrays {
+
+}
